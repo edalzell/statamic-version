9481d072f445367f347c458d7424be1d3f6f5c6a
Erin Dalzell <erin@thedalzells.org>
2018-08-11 20:45:37 -0700

    Make link to repos clearer

--- Versions/resources/views/outdated-addons.blade.php
+++ Versions/resources/views/outdated-addons.blade.php
@@ -6,12 +6,14 @@
             <th>Addon</th>
             <th>Your Version</th>
             <th>Current Version</th>
+            <th>Link to repo</th>
         </tr>
     @foreach ($addons as $addon)
         <tr>
             <td>{{ $addon->get('name') }}</td>
             <td>{{ $addon->get('version') }}</td>
-            <td><a href="{{ $addon->get('url') }}">{{ $addon->get('latest_version') }}</a></td>
+            <td>{{ $addon->get('latest_version') }}</a></td>
+            <td><a href="{{ $addon->get('url') }}">{{ $addon->get('url') }}</a>
         </tr>
     @endforeach
     </table>
